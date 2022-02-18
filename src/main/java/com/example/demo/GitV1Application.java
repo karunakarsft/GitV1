ba6160e76eddbc442ee385d027c972f6ce94fd77
karunakarsft <50539174+karunakarsft@users.noreply.github.com>
2022-02-18 17:55:20 +0530

    first change

--- src/main/java/com/example/demo/GitV1Application.java
+++ src/main/java/com/example/demo/GitV1Application.java
@@ -8,6 +8,8 @@ public class GitV1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitV1Application.class, args);
+		
+		System.out.println("This is my first GIT project !");
 	}
 
 }
